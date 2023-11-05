1852d5c9b8148bbbea1ce48229898ef6a1e94bc8
Lucas Siqueira <lucassiqueirafernandes07@gmail.com>
2023-11-05 17:30:40 -0300

    Setting ActiveStorage

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,6 @@ class Lesson < ApplicationRecord
   belongs_to :lesson_class
 
   validates :title, length: { maximum: 30 }
+
+  has_one_attached :video
 end
--- app/models/lesson_class.rb
+++ app/models/lesson_class.rb
@@ -1,4 +1,18 @@
 class LessonClass < ApplicationRecord
   validates :title, length: { maximum: 30 }
   validates :description, length: { maximum: 50 }
+
+  has_one_attached :background
+
+  # TODO:
+  # Create controllers and connect ActiveStorage with views
+  # Letting the following example how to do It:
+  # 
+  # Controller:
+  # lesson_class = LessonClass.new(title: 'Exemplo de Aula')
+  # lesson_class.background.attach(io: File.open('path/to/your/image.jpg'), filename: 'image.jpg')
+  # lesson_class.save
+  # 
+  # Views:
+  # <%= image_tag rails_blob_path(lesson_class.background, disposition: "attachment"), alt: "Lesson Class Image" %>
 end
