8c8f78a738ac5cc78176e72824780539bf808379
Lucas Siqueira <lucassiqueirafernandes07@gmail.com>
2023-11-09 17:39:47 -0300

    Setting homepage

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './public/*.html',
@@ -10,7 +12,23 @@ module.exports = {
       backgroundImage: {
         'main-image': "url('backgrounds/background.jpg')",
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
