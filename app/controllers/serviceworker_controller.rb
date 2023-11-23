5611da4b3870a2e74eaad7ab997a671a3fd242e1
Lucas Fernandes <lucassiqueirafernandes07@gmail.com>
2023-11-23 11:53:22 -0300

    Setting CORS

--- app/controllers/serviceworker_controller.rb
+++ app/controllers/serviceworker_controller.rb
@@ -1,4 +1,6 @@
 class ServiceworkerController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:serviceworker, :manifest]
+
   def serviceworker
     render file: "public/serviceworker.js", content_type: 'application/javascript', layout: false
   end
