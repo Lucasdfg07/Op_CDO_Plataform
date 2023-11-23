b1370502d4502382cf22fef2fbd437b9c4f44f9f
Lucas Fernandes <lucassiqueirafernandes07@gmail.com>
2023-11-23 03:05:00 -0300

    Setting pwa_banner local storage

--- app/javascript/pwa-banner.js
+++ app/javascript/pwa-banner.js
@@ -1,31 +1,47 @@
-function isRunningAsPWA() {
-  return (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://');
+function wasBannerRecentlyClosed() {
+  const bannerClosedTime = localStorage.getItem('bannerClosedTime');
+  if (!bannerClosedTime) {
+    return false;
+  }
+
+  const timeElapsed = new Date().getTime() - new Date(bannerClosedTime).getTime();
+  return timeElapsed < 3 * 24 * 60 * 60 * 1000; // 3 dias em milissegundos
 }
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Não exibe o banner se estiver rodando como PWA
-  if (isRunningAsPWA()) {
-    console.log('Rodando como um PWA, banner não será exibido.');
-    return;
-  }
-
-  e.preventDefault();
-  deferredPrompt = e;
-  const installBanner = document.getElementById('install_banner');
-  if (installBanner && window.innerWidth <= 500) {
-    installBanner.style.display = 'flex';
-  }
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const installBanner = document.getElementById('install_banner');
-  if (installBanner) {
-    installBanner.addEventListener('click', () => {
-      if (deferredPrompt) { // Verifica se deferredPrompt foi definido
+  const closeBannerButton = document.getElementById('close_banner');
+
+  if (wasBannerRecentlyClosed()) {
+    if (installBanner) {
+      installBanner.style.display = 'none';
+    }
+    return;
+  }
+
+  let deferredPrompt;
+  window.addEventListener('beforeinstallprompt', (e) => {
+    if (isRunningAsPWA()) {
+      return;
+    }
+
+    e.preventDefault();
+    deferredPrompt = e;
+    if (installBanner && window.innerWidth <= 500) {
+      installBanner.style.display = 'flex';
+    }
+  });
+
+  if (installBanner && closeBannerButton) {
+    closeBannerButton.addEventListener('click', () => {
+      installBanner.style.display = 'none';
+      localStorage.setItem('bannerClosedTime', new Date().toISOString());
+    });
+
+    if (deferredPrompt) {
+      installBanner.addEventListener('click', () => {
         installBanner.style.display = 'none';
-        deferredPrompt.prompt(); // Mostra o prompt de instalação
+        deferredPrompt.prompt();
         deferredPrompt.userChoice.then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
             console.log('Usuário aceitou a instalação do PWA');
@@ -34,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
           }
           deferredPrompt = null;
         });
-      }
-    });
+      });
+    }
   }
 });
