4a6656ea1903c81c66d3df1a9fba01e167bf1030
Lucas Fernandes <lucassiqueirafernandes07@gmail.com>
2023-11-23 23:22:17 -0300

    Setting PWA Banner validation for cookie

--- app/controllers/serviceworker_controller.rb
+++ app/controllers/serviceworker_controller.rb
@@ -1,5 +1,6 @@
 class ServiceworkerController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:serviceworker, :manifest]
+  skip_before_filter :authenticate_user!
 
   def serviceworker
     render file: "public/serviceworker.js", content_type: 'application/javascript', layout: false
