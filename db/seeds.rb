e8d12541bd650d5239454f7fe018fe82db7d59c3
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2023-11-26 22:17:58 -0300

    Adjusting some bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 file_path = Rails.root.join('db', 'seeds', 'op_cdo_users.txt')
 emails = File.readlines(file_path).map(&:strip).reject(&:empty?)
 
-password = '2024NAGRINGA'
-
 emails.each do |email|
-  User.create(email: email, password: password, password_confirmation: password) unless User.exists?(email: email)
+  User.create(email: email, password: ENV['DEFAULT_PASSWORD']) unless User.exists?(email: email)
 end
